89a850aa3e4395bfb05cafb3f34073dc220d5aea
GuillermoMuniz <guillemufer@gmail.com>
2024-05-13 14:10:05 +0200

    comenzamos configurando el JavaScrip

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,1 +1,14 @@
-console.log("Hola Carla");
+const urlObtenerEmpleados = "http:/192.168.50.55/backend/conexionBBDD.php";
+
+async function obtenerEmpleados(){
+    try{
+        //Obtener la lista de enpleados del backend
+        const listaEmpleados = await fetch(urlObtenerEmpleados)
+        .then(respuesta => respuesta.json())
+
+        console.log(respuesta)
+    }catch(error){
+        console.log(error)
+    }
+
+}
